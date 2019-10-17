065017de4ebda21ffc64f7becee0a0e9347b95ed
DarkestEntropy <entropial@protonmail.ch>
2019-10-17 08:35:13 +0400

    Replaced hardcoded quotes positions with finding loops

--- Crack/decompress_zlib.py
+++ Crack/decompress_zlib.py
@@ -11,9 +11,23 @@ workdir.mkdir(exist_ok=True)
 # Work on original Base64-encoded + zlib-compressed file
 # In the end we get raw python source, yet obfuscated
 fd = open(Path(workdir, 'bomber.py.source'))
-fd.seek(59)
+while True:
+    s = fd.read(16)
+    index = s.find('\'')
+    if index >= 0:
+        fd.seek(fd.tell() - (len(s) - index - 1))
+        break
 
-compressed = fd.read(3218328)
+compressed = str()
+while True:
+    s = fd.read(512)
+    index = s.find('\'')
+    if index < 0:
+        compressed += s
+    else:
+        compressed += s[:index]
+        break
+
 open(Path(workdir, 'bomber.py.compressed'), 'w').write(compressed)
 
 decoded = base64.b64decode(compressed)
